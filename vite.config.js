825d05ef4e6c034be4f19471f13fa76300e096fd
Noel Delgado <auhcsei@gmail.com>
2022-11-19 19:58:18 -0700

    config: add demos entry points

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        demo01: resolve(__dirname, 'demos/01-svg-gsap/index.html'),
+        demo02: resolve(__dirname, 'demos/02-canvas/index.html'),
+      }
+    }
+  }
 })
